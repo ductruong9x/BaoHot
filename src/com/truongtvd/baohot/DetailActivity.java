dc5476b18d6b379c96c87a9be6c8e57e330a0818
Truong Vu Duc Truong <ductruongcntt@gmail.com>
2014-06-03 14:08:15 +0700

    xxx

--- src/com/truongtvd/baohot/DetailActivity.java
+++ src/com/truongtvd/baohot/DetailActivity.java
@@ -1,16 +1,28 @@
 package com.truongtvd.baohot;
 
+import java.util.Arrays;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.text.Html;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
-import com.truongtvd.baohot.adapter.ViewPagerAdapter;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.Session.NewPermissionsRequest;
+import com.facebook.Session.StatusCallback;
+import com.facebook.SessionState;
 import com.truongtvd.baohot.adapter.ViewPagerDetailAdapter;
 import com.viewpagerindicator.PagerSlidingTabStrip;
 
@@ -20,6 +32,8 @@ public class DetailActivity extends SherlockFragmentActivity {
 	private ViewPager vpMain;
 	private PagerSlidingTabStrip mIndicator;
 	private ViewPagerDetailAdapter adapter;
+	private ProgressDialog dialog;
+	private String link, title, des, image;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +48,16 @@ public class DetailActivity extends SherlockFragmentActivity {
 		actionBar.setIcon(android.R.color.transparent);
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
-
+		dialog = new ProgressDialog(this);
+		dialog.setMessage("Sharing...");
+		Intent intent = getIntent();
+		link = intent.getStringExtra("LINK");
+		title = intent.getStringExtra("TITLE");
+		des = intent.getStringExtra("DES");
+		image = intent.getStringExtra("IMAGE");
 		vpMain = (ViewPager) findViewById(R.id.vpMain);
 		mIndicator = (PagerSlidingTabStrip) findViewById(R.id.indicatorTabHome);
-		mIndicator.setBackgroundDrawable(new ColorDrawable(Color
-				.parseColor("#7e3794")));
+		mIndicator.setBackgroundResource(R.color.app_color);
 		mIndicator.setIndicatorColor(Color.parseColor("#ffffff"));
 		mIndicator.setTextColor(Color.WHITE);
 		adapter = new ViewPagerDetailAdapter(DetailActivity.this,
@@ -51,13 +70,73 @@ public class DetailActivity extends SherlockFragmentActivity {
 	}
 
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getSupportMenuInflater().inflate(R.menu.detail, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
 		switch (item.getItemId()) {
 		case android.R.id.home:
 			finish();
 			break;
+		case R.id.menu_share:
+
+			try {
+				if (!Session.getActiveSession().getPermissions()
+						.contains("publish_actions")) {
+					NewPermissionsRequest request = new NewPermissionsRequest(
+							DetailActivity.this,
+							Arrays.asList("publish_actions"));
+					request.setCallback(new StatusCallback() {
+
+						@Override
+						public void call(Session session, SessionState state,
+								Exception exception) {
+							// TODO Auto-generated method stub
+
+						}
+					});
+
+					Session.getActiveSession().requestNewPublishPermissions(
+							request);
+					return true;
+				}
+			} catch (Exception e) {
+
+			}
+			dialog.show();
+			Bundle postParams = new Bundle();
+			postParams.putString("name", title);
+			postParams
+					.putString(
+							"message",
+							"Link tải ứng dụng Báo Hót cho Android: "
+									+ "https://play.google.com/store/apps/details?id=com.truongtvd.baohot");
+			postParams.putString("description", des);
+			postParams.putString("picture", image);
+			postParams.putString("link", link);
+
+			Request.Callback callback = new Request.Callback() {
+				public void onCompleted(Response response) {
+					dialog.dismiss();
+					Toast.makeText(DetailActivity.this, "Share successfuly	",
+							Toast.LENGTH_SHORT).show();
+				}
+			};
+
+			Request request = new Request(Session.getActiveSession(),
+					"me/feed", postParams, HttpMethod.POST, callback);
+
+			RequestAsyncTask task = new RequestAsyncTask(request);
+			task.execute();
+
+			break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 }
